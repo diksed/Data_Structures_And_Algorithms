a95057a18af85249a9827764651dfc0f21e3cb5c
diksed <diksed00@gmail.com>
2022-06-12 05:30:05 +0300

    DoublyLinkedList Remove

--- LinkedList/DoblyLinkedList/DoublyLinkedList.cs
+++ LinkedList/DoblyLinkedList/DoublyLinkedList.cs
@@ -52,11 +52,134 @@ namespace LinkedList.DoblyLinkedList
             Count++;
             return;
         }
+        public void AddBefore(DbNode<T> node, T value)
+        {
+            if (node == null || value is null)
+                throw new ArgumentNullException();
+            if (isHeadNull || node.Equals(Head))
+            {
+                AddFirst(value);
+                return;
+            }
+            var newNode = new DbNode<T>(value);
+            var current = Head;
+            var prev = current;
+            while (current != null)
+            {
+                if (current.Equals(node))
+                {
+                    newNode.Next = prev.Next;
+                    newNode.Prev = prev;
+                    prev.Next = newNode;
+                    newNode.Next.Prev = newNode;
+                    Count++;
+                    return;
+                }
+                prev = current;
+                current = current.Next;
+            }
+            throw new ArgumentException("There is no such a node in the list.");
+        }
+        public void AddAfter(DbNode<T> node, T value)
+        {
+            if (node == null)
+                throw new ArgumentNullException(); 
+            if (isHeadNull)
+            {
+                AddFirst(value);
+                return;
+            }
+            var newNode = new DbNode<T>(value);
+            var current = Head;
+            while (current != null)
+            {
+                if (current.Equals(node))
+                {
+                    if (current.Next != null)
+                    {
+                        newNode.Next = current.Next;
+                        current.Next = newNode;
+                        newNode.Prev = current;
+                        newNode.Next.Prev = newNode;
+                        Count++;
+                        return;
+                    }
+                    else
+                    {
+                        AddLast(value);
+                        return;
+                    }
+                }
+                current = current.Next;
+            }
+            throw new ArgumentException("There is no such a node in the list.");
+        }
+        public T RemoveFirst()
+        {
+            if (isHeadNull)
+                throw new Exception(nameof(Head));
+            var temp = Head;
+            if(Count == 1)
+            {
+                Head = null;
+                Count--;
+                return temp.Value;
+            }
+            Head = Head.Next;
+            Head.Prev = null;
+            Count--;
+            return temp.Value;
+        }
+        public T RemoveLast()
+        {
+            if (isHeadNull || Count == 0)
+                throw new Exception();
+            if(Count == 1)
+            {
+                var temp = Head;
+                Head = null;
+                Count--;
+                return temp.Value;
+            }
+            else
+            {
+                var temp = Tail;
+                Tail.Prev.Next = null;
+                Tail = Tail.Prev;
+                Count--;
+                return temp.Value;
+            }
+        }
+        public T Remove(T value)
+        {
+            if (isHeadNull)
+                throw new Exception("The list is empty.");
+            var current = Head;
+            var prev = current;
+            while(current != null)
+            {
+                if (current.Value.Equals(value))
+                {
+                    if (current.Value.Equals(Head.Value))
+                        return RemoveFirst();
+                    if(current.Value.Equals(Tail.Value))
+                        return RemoveLast();
+                    var temp = current;
+                    prev.Next = current.Next;
+                    current.Next.Prev = current.Prev;
+                    current = null;
+                    Count--;
+                    return temp.Value;
+                }
+                prev = current;
+                current = current.Next;
+            }
+            throw new ArgumentException("There is no such a this node in the list.");
+        }
         public IEnumerator<T> GetEnumerator()
         {
             return new DoublyLinkedListEnumerator<T>(Head);
         }
-
         IEnumerator IEnumerable.GetEnumerator()
         {
             return GetEnumerator();
--- LinkedListTests/DoublyLinkedListTests.cs
+++ LinkedListTests/DoublyLinkedListTests.cs
@@ -1,4 +1,5 @@
 ﻿using LinkedList.DoblyLinkedList;
+using System;
 using Xunit;
 
 namespace LinkedListTests
@@ -13,7 +14,9 @@ namespace LinkedListTests
         [Theory]
         [InlineData('f')]
         [InlineData('e')]
-        [InlineData('r')]
+        [InlineData('m')]
+        [InlineData('o')]
+        [InlineData('t')]
         public void AddFirst_Test(char value)
         {
             _list.AddFirst(value);
@@ -42,5 +45,73 @@ namespace LinkedListTests
                 item => Assert.Equal(item, 'z'),
                 item => Assert.Equal(item, value));
         }
+        [Theory]
+        [InlineData('k')]
+        [InlineData('m')]
+        [InlineData('n')]
+        [InlineData('r')]
+        [InlineData('s')]
+        public void AddBefore_Test(char value)
+        {
+            _list.AddBefore(_list.Head.Next, value);
+
+            Assert.Equal(value, _list.Head.Next.Value);
+
+            Assert.Collection(_list,
+                item => Assert.Equal(item, 'a'),
+                item => Assert.Equal(item, value),
+                item => Assert.Equal(item, 'z'));
+        }
+        [Fact]
+        public void AddBefore_ArgumentException_Test()
+        {
+            DbNode<char> node = new DbNode<char>('x');
+
+            Assert.Throws<ArgumentException>(() =>
+            _list.AddBefore(node, node.Value));
+        }
+        [Theory]
+        [InlineData('k')]
+        [InlineData('m')]
+        [InlineData('n')]
+        [InlineData('r')]
+        [InlineData('s')]
+        public void AddAfter_Test(char value)
+        {
+            _list.AddAfter(_list.Head.Next, value);
+
+            Assert.Equal(value, _list.Tail.Value);
+
+            Assert.Collection(_list,
+                item => Assert.Equal(item, _list.Head.Value),
+                item => Assert.Equal(item, _list.Head.Next.Value),
+                item => Assert.Equal(value, _list.Tail.Value));
+        }
+        [Fact]
+        public void RemoveFirst_Test()
+        {
+            _list.RemoveFirst();
+
+            Assert.Collection(_list, item => Assert.Equal(item, 'z'));
+        }
+        [Fact]
+        public void RemoveLast_Exception_Test()
+        {
+            var r1 = _list.RemoveLast();
+            var r2 = _list.RemoveLast();
+
+            Assert.Equal(r1, 'z');
+            Assert.Equal(r2, 'a');
+            Assert.Throws<Exception>(() => _list.RemoveLast());
+        }
+        [Fact]
+        public void Remove_Exception_Test()
+        {
+            _list.Remove('a');
+            _list.Remove('z');
+
+            Assert.Throws<Exception>(() => _list.Remove('x'));
+        }
+
     }
 }
