9c280e4818eb527cb8512a9d30ceae5d4fc1f8d4
diksed <diksed00@gmail.com>
2022-06-12 21:51:39 +0300

    QueueApp

---
+++ QueueApp/Program.cs
@@ -0,0 +1,23 @@
+﻿var _queue = new Queue<ToDo>();
+_queue.Enqueue(new ToDo()
+{
+    Time = 2,
+    Describe = "Okula gidilecek."
+});
+_queue.Enqueue(new ToDo()
+{
+    Time = 1,
+    Describe = "Yemek yenilecek."
+});
+_queue.Enqueue(new ToDo()
+{
+    Time = 3,
+    Describe = "Sınava girilecek."
+});
+var result = _queue.Dequeue();
+Console.WriteLine(result + " yapıldı.");
+Console.WriteLine(_queue.Count);
+foreach (var item in _queue)
+{
+    Console.WriteLine(item);
+}
