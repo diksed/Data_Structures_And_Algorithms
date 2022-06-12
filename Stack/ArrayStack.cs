9a156453b946a0e1a76930b6168e87160535f138
diksed <diksed00@gmail.com>
2022-06-12 20:41:18 +0300

    Stack and ArrayStack

---
+++ Stack/ArrayStack.cs
@@ -0,0 +1,38 @@
+﻿using DataStructures.Array.Generic;
+using DataStructures.Stack.Contracts;
+using System.Collections;
+
+
+namespace DataStructures.Stack
+{
+    public class ArrayStack<T> : IStack<T>
+    {
+        private readonly Array<T> _Array = new Array<T>();
+        public int Count => _Array.Count;
+        public IEnumerator<T> GetEnumerator()
+        {
+            return _Array.GetEnumerator();
+        }
+        public T Peek()
+        {
+            if(Count == 0)
+                return default(T);
+            return _Array.GetValue(_Array.Count - 1);
+        }
+        public T Pop()
+        {
+            if (Count == 0)
+                throw new Exception("Empty stack.");
+            var result = _Array.Remove();
+            return result;
+        }
+        public void Push(T item)
+        {
+            _Array.Add(item);
+        }
+        IEnumerator IEnumerable.GetEnumerator()
+        {
+            return GetEnumerator();
+        }
+    }
+}
---
+++ StackApp/Program.cs
@@ -0,0 +1,9 @@
+﻿using DataStructures.Stack;
+
+string message = "Selam";
+var stack = new ArrayStack<char>();
+for (int i = 0; i < message.Length; i++)
+    stack.Push(message[i]);
+var n = stack.Count;
+for (int i = 0; i < n; i++)
+    Console.WriteLine(stack.Pop());
