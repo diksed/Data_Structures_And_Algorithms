eea3e2025f3744fb11fd134071e025a3998448dd
diksed <diksed00@gmail.com>
2022-06-12 04:44:00 +0300

    DoublyLinkedList AddFirst

---
+++ LinkedListTests/DoublyLinkedListTests.cs
@@ -0,0 +1,24 @@
+﻿using LinkedList.DoblyLinkedList;
+using Xunit;
+
+namespace LinkedListTests
+{
+    public class DoublyLinkedListTests
+    {
+        private DoublyLinkedList<char> _list;
+        public DoublyLinkedListTests()
+        {
+            _list = new DoublyLinkedList<char>(new char[] {'a','z'});
+        }
+        [Theory]
+        [InlineData('f')]
+        [InlineData('e')]
+        [InlineData('r')]
+        public void AddFirst_Test(char value)
+        {
+            _list.AddFirst(value);
+
+            Assert.Equal(value, _list.Head.Value);
+        }
+    }
+}
