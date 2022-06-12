7a73c5273a40a3de170ed1c34fae86a2da0e1fe9
diksed <diksed00@gmail.com>
2022-06-12 21:44:01 +0300

    Queue

---
+++ Queue/Queue.cs
@@ -0,0 +1,31 @@
+﻿using DataStructures.Queue.Contracts;
+using System.Collections;
+
+namespace Queue
+{
+    public class Queue<T> : IQueue<T>
+    {
+        private readonly IQueue<T> _queue;
+        public int Count => _queue.Count;
+        public T DeQueue()
+        {
+            return _queue.DeQueue();
+        }
+        public void EnQueue(T value)
+        {
+            _queue.EnQueue(value);
+        }
+        public IEnumerator<T> GetEnumerator()
+        {
+            return _queue.GetEnumerator();
+        }
+        public T Peek()
+        {
+            return _queue.Peek();
+        }
+        IEnumerator IEnumerable.GetEnumerator()
+        {
+            return GetEnumerator();
+        }
+    }
+}
