48fefcd150c7129b7badf338f72ed9cce3205a06
diksed <diksed00@gmail.com>
2022-06-13 03:18:47 +0300

    Bubble Sort

---
+++ SortingAlgorithmsApps/Program.cs
@@ -0,0 +1,13 @@
+﻿var arr = new int[] { 40, 50, 10, 30, 20 };
+PrintArray(arr);
+SortingAlgorithms.BubbleSort.Sort(arr);
+PrintArray(arr);
+static void PrintArray<T>(T[] arr)
+{
+    Console.WriteLine();
+    foreach (var item in arr)
+    {
+        Console.Write($"{item,-5}");
+    }
+    Console.WriteLine();
+}
