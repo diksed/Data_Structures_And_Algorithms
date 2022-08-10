2e95841bdef3e3295a13d5853cabffe9e5fd77f8
diksed <diksed00@gmail.com>
2022-08-10 17:01:13 +0300

    BinaryTrees

---
+++ BinaryTreesApp/Program.cs
@@ -0,0 +1,11 @@
+﻿using DataStructures.Trees.BinaryTree;
+
+var root = new Node<int>(1);
+
+root.Left = new Node<int>(2);
+root.Right = new Node<int>(3);
+root.Left.Left = new Node<int>(4);
+root.Left.Right = new Node<int>(5);
+root.Right.Left = new Node<int>(6);
+root.Right.Right = new Node<int>(7);
+
